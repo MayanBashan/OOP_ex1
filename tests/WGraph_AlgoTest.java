09490a5bbf0f1c1fa8d7977bb06158a84754d512
Mayan <mayanbashan@gmail.com>
2020-11-18 19:31:21 +0200

    added tests for shortestpath, shortestpathdist

--- tests/WGraph_AlgoTest.java
+++ tests/WGraph_AlgoTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WGraph_AlgoTest {
@@ -173,17 +177,234 @@ class WGraph_AlgoTest {
         assertFalse(graph_algo.isConnected());
     }
 
+
     @Test
     void shortestPathDist() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,1);
+        graph.connect(4,5,9);
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        double actual_dist = graph_algo.shortestPathDist(2,5);
+
+        assertEquals(10,actual_dist);
     }
 
     @Test
+    void shortestPathDist2() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,10);
+        graph.connect(4,5,9);
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        double actual_dist = graph_algo.shortestPathDist(2,5);
+
+        assertEquals(13,actual_dist);
+    }
+
+    @Test
+    void shortestPathDist3() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,10);
+        graph.connect(4,5,9);
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        double actual_dist = graph_algo.shortestPathDist(3,3);
+
+        assertEquals(0,actual_dist);
+    }
+
+    @Test
+    void shortestPathDist4() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,10);
+        graph.connect(4,5,9);
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        double actual_dist = graph_algo.shortestPathDist(1,6);
+
+        assertEquals(-1,actual_dist);
+    }
+
+    @Test
+    void shortestPathDist5() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,10);
+        graph.connect(4,5,9);
+        graph.connect(6,7,100);
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        double actual_dist = graph_algo.shortestPathDist(7,6);
+
+        assertEquals(100,actual_dist);
+    }
+
+
+    @Test
     void shortestPath() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,10);
+        graph.connect(4,5,9);
+
+        List<node_info> expected_lst = new LinkedList<>();
+        expected_lst.add(graph.getNode(5));
+        expected_lst.add(graph.getNode(4));
+        expected_lst.add(graph.getNode(0));
+        expected_lst.add(graph.getNode(1));
+        expected_lst.add(graph.getNode(2));
+        Collections.reverse(expected_lst);
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        List<node_info> actual_list = graph_algo.shortestPath(2,5);
+
+        assertEquals(expected_lst,actual_list);
+    }
+
+    @Test
+    void shortestPath2() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,1);
+        graph.connect(4,5,9);
+
+        List<node_info> expected_lst = new LinkedList<>();
+        expected_lst.add(graph.getNode(2));
+        expected_lst.add(graph.getNode(4));
+        expected_lst.add(graph.getNode(5));
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        List<node_info> actual_list = graph_algo.shortestPath(2,5);
+
+        assertEquals(expected_lst,actual_list);
+    }
+
+    @Test
+    void shortestPath3() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,1);
+        graph.connect(4,5,9);
+        graph.connect(6,7,100);
+
+        List<node_info> expected_lst = new LinkedList<>();
+        expected_lst.add(graph.getNode(6));
+        expected_lst.add(graph.getNode(7));
+
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        List<node_info> actual_list = graph_algo.shortestPath(6,7);
+
+        assertEquals(expected_lst,actual_list);
+    }
+
+    @Test
+    void shortestPath4() {
+        weighted_graph graph = new WGraph_DS();
+        for (int i=0; i<=7; i++){
+            graph.addNode(i);
+        }
+        graph.connect(0,1,1);
+        graph.connect(0,3,3);
+        graph.connect(0,4,1);
+        graph.connect(1,2,2);
+        graph.connect(1,3,6);
+        graph.connect(2,4,1);
+        graph.connect(4,5,9);
+        graph.connect(6,7,100);
+
+        List<node_info> expected_lst = new LinkedList<>();
+        expected_lst.add(graph.getNode(1));
+        expected_lst.add(graph.getNode(0));
+        expected_lst.add(graph.getNode(3));
+
+
+        WGraph_Algo graph_algo = new WGraph_Algo(graph);
+
+        List<node_info> actual_list = graph_algo.shortestPath(1,3);
+
+        assertEquals(expected_lst,actual_list);
+        assertEquals(4,graph_algo.shortestPathDist(1,3));
     }
 
     @Test
     void save() {
-
+        weighted_graph g = new WGraph_DS();
+        this.get_graph();
+        weighted_graph loaded = new WGraph_DS();
+        WGraph_Algo big = new WGraph_Algo(loaded);
+        big.save(filename);
+        big.load(filename);
+        boolean flag = loaded.equals(big);
+        assertTrue(flag);
     }
 
     @Test
